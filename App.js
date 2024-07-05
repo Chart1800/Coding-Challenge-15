5b7cf4ca6957037c0da88bf6b02bbfedf5ff7150
Chart1800 <cameronh11@usf.edu>
2024-07-05 14:16:31 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+// U94741303
+
+const AboutMe = () => {
+  return (
+    <div className= "AboutMe">
+      <h1>Cameron Hart</h1>
+      <p>I am 24 years old, born and raised in Tampa Florida. I have 3 siblings, two older sisters and one younger brother. Recently, Ive been non-stop with a now 5 month old baby girl.
+     In my free time though, my favorite hobbies inlcude taking my boat out fishing and going to Weeeki Wachee Springs. </p>
+     <h2>Contact Info</h2>
+     <p>Email: Cameronh11@usf.edu</p>
+     <p>LinkedIn: <a href="https://www.linkedin.com/in/cameron-hart-a59a82317/">Cameron Hart</a></p>
+     <h3>Professional Skills</h3>
+     <p>-Strategic Planning</p>
+      <p>-Problem Solving</p>
+      <p>-Time Management</p>
+      <p>-Strong Commmunication</p>
+      <h4> Work Experience</h4>
+      <p> Red Lobster - Waiter/Server (2017-Present)</p>
+      <p> As a waiter of a well-known chain seafood restaurant, my job entails providing guests with a great dining experience, while performing mutliple tasks in a timely manner. </p>
+      <h5> Education</h5>
+      <p>    - Springstead Highschool- Diploma (2014-2018)</p>
+      <p>    - Pasco Hernando State College(PHSC)- AA in Arts (2019-2022)</p>
+      <p>    - University of South FLorida(USF)- Majoring in Business Analytics & Information Systems (2023-Present) - Junior </p>
+     </div>
+  );
+};
+
+export default AboutMe;
+
